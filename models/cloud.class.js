0bc2b4af15f6f3fec757f0ff19d007be2bd880ab
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-01 14:11:42 +0200

    feat: Cloud

---
+++ models/cloud.class.js
@@ -0,0 +1,6 @@
+class Cloud extends MovableObject {
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.x = 200 + Math.random() * 500;}}
--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,9 @@ class World {
     new Chicken(),
     new Chicken(),
   ];
+  clouds = [
+    new Cloud()
+  ];
   canvas;
   ctx;
 
@@ -36,6 +39,15 @@ class World {
           enemy.y,
           enemy.width,
           enemy.height);
+      })
+      
+          this.clouds.forEach(cloud => {
+        this.ctx.drawImage(
+          cloud.img,
+          cloud.x,
+          cloud.y,
+          cloud.width,
+          cloud.height);
       })
 
     let self = this;
