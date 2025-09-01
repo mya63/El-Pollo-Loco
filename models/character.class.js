7c0246ae2e06256fef8c6935208e015bb3fbc9dc
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-01 13:56:31 +0200

    feat: chicken random math

--- models/character.class.js
+++ models/character.class.js
@@ -1,8 +1,7 @@
 class Character extends MovableObject {
   constructor() {
     super().loadImage(
-      'img/2_character_pepe/2_walk/W-21.png'
-    );
+      'img/2_character_pepe/2_walk/W-21.png');
   }
 
   jump() {}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,9 +1,9 @@
 class MovableObject {
   x = 120;
   y = 250;
-  img;
   height = 150;
   width = 100;
+  img;
 
   loadImage(path) {
     this.img = new Image();
@@ -14,5 +14,7 @@ class MovableObject {
     console.log('Moving right');
   }
 
-  moveLeft() {}
+  moveLeft() {
+    console.log('Moving left');
+  }
 }
