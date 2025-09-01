047ff2a8122f1a14679109fd23653d479136534d
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-01 20:07:36 +0200

    feat: cloud

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,6 +1,14 @@
 class Cloud extends MovableObject {
+  y = 20;
+  height = 250;
+  width = 500;
+  
+  constructor() {
+    super().loadImage(
+      'img/5_background/layers/4_clouds/1.png'
+    );
 
-    constructor() {
-        super().loadImage('img/5_background/layers/4_clouds/1.png');
+    this.x = 0 + Math.random() * 500;
 
-        this.x = 200 + Math.random() * 500;}}
+  }
+}
