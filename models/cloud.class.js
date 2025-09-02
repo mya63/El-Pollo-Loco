1f9918a1d3ac625c642ba584bd1be6b69d98dcaf
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-02 10:18:01 +0200

    feat: Wolken bewegen 60 fps

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -8,7 +8,14 @@ class Cloud extends MovableObject {
       'img/5_background/layers/4_clouds/1.png'
     );
 
-    this.x = 0 + Math.random() * 500;
+    this.x = Math.random() * 500;
+    this.animate();
 
   }
+
+  animate() {
+    setInterval(() => {
+    this.x -= 0.15;
+  }, 1000 / 60);
+}
 }
