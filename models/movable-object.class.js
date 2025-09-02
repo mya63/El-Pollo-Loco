87114acc885efb3fd316b8f281c74b0d3edbc9d3
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-02 10:45:08 +0200

    feat: ImageCache

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,13 +1,24 @@
 class MovableObject {
   x = 120;
   y = 280;
+  img;
   height = 150;
   width = 100;
-  img;
+  imageCache = [];
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+ 
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = path;
+    });
   }
 
   moveRight() {
