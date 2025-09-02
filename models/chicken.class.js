1a250c5abfdd4f16544ed960365b802beb907970
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-02 19:39:39 +0200

    feat: Tempo random

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -13,6 +13,8 @@ class Chicken extends MovableObject {
     );
     this.loadImages(this.IMAGES_WALKING);
     this.x = 200 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 0.5;
+
     this.animate();
   }
   animate() {
