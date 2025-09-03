560e6470a4a2cb8bafaeb0e0baeb1a7eb19c5ffb
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-03 17:33:20 +0200

    feat: Kamera verschieben

--- models/world.class.js
+++ models/world.class.js
@@ -24,6 +24,7 @@ class World {
   canvas;
   ctx;
   keyboard;
+  camera_x = 0;
 
 
   constructor(canvas, keyboard) {
@@ -41,10 +42,14 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x, 0);
+
     this.addObjectsToMap(this.backgroundObjects);
     this.addToMap(this.character);
     this.addObjectsToMap(this.enemies);
     this.addObjectsToMap(this.clouds);
+
+    this.ctx.translate(-this.camera_x, 0);
 
     let self = this;
     requestAnimationFrame(function () {
