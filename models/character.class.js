5d03da405937c822c472ca8c2ac84331c0fa0136
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-03 14:52:20 +0200

    feat: Bewegen

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject {
   height = 250;
   y = 180;
+  speed = 6;
   IMAGES_WALKING = [
     'img/2_character_pepe/2_walk/W-21.png',
     'img/2_character_pepe/2_walk/W-22.png',
@@ -19,14 +20,29 @@ class Character extends MovableObject {
   }
 
   animate() {
+
     setInterval(() => {
       if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+      }
+
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
+        this.x += this.speed;
+
+
         let i = this.currentImage % this.IMAGES_WALKING.length;
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageCache[path];
         this.currentImage++;
       }
-    }, 1000);
+    }, 50);
   }
 
   jump() {}
