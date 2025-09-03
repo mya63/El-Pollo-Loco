e3a5a305d785ccc6d7348e8fa0c45767e05b8652
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-03 21:03:23 +0200

    feat: Character

--- models/character.class.js
+++ models/character.class.js
@@ -27,11 +27,11 @@ class Character extends MovableObject {
         this.otherDirection = false;
       }
 
-      if (this.world.keyboard.LEFT) {
+      if (this.world.keyboard.LEFT && this.x > 0) {
         this.x -= this.speed;
         this.otherDirection = true;
       }
-      this.world.camera_x = -this.x;
+      this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
     setInterval(() => {
