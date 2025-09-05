1aa7169fcf7783cb12a8b0038960010a82e7168a
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-05 09:32:31 +0200

    feat: Springen

--- models/character.class.js
+++ models/character.class.js
@@ -10,12 +10,27 @@ class Character extends MovableObject {
     'img/2_character_pepe/2_walk/W-25.png',
     'img/2_character_pepe/2_walk/W-26.png'
   ];
+
+  IMAGES_JUMPING = [
+    'img/2_character_pepe/3_jump/J-31.png',
+    'img/2_character_pepe/3_jump/J-32.png',
+    'img/2_character_pepe/3_jump/J-33.png',
+    'img/2_character_pepe/3_jump/J-34.png',
+    'img/2_character_pepe/3_jump/J-35.png',
+    'img/2_character_pepe/3_jump/J-36.png',
+    'img/2_character_pepe/3_jump/J-37.png',
+    'img/2_character_pepe/3_jump/J-38.png',
+    'img/2_character_pepe/3_jump/J-39.png',
+    'img/2_character_pepe/3_jump/J-40.png'
+  ];
+
   world;
 
   constructor() {
     super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     this.loadImages(this.IMAGES_WALKING);
-this.applyGravity();
+    this.loadImages(this.IMAGES_JUMPING);
+    this.applyGravity();
     this.animate();
   }
 
@@ -38,10 +53,16 @@ this.applyGravity();
     }, 1000 / 60);
 
     setInterval(() => {
+
+      if(this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
+
       if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
         this.playAnimation(this.IMAGES_WALKING);
       }
-    }, 50);
+    }
+   }, 50);
   }
 
   jump() {}
