af89cfb3328e19b0a3d2d17e20fc70a962a00482
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-05 20:22:05 +0200

    refactor

--- models/world.class.js
+++ models/world.class.js
@@ -44,10 +44,7 @@ class World {
 
   addToMap(mo) {
     if (mo.otherDirection) {
-      this.ctx.save();
-      this.ctx.translate(mo.width, 0);
-      this.ctx.scale(-1, 1);
-      mo.x = mo.x * -1;
+     this.flipImage(mo);
     }
     
 mo.draw(this.ctx);
@@ -55,8 +52,19 @@ mo.drawFrame(this.ctx);
 
 
     if (mo.otherDirection) {
-      mo.x = mo.x * -1;
-      this.ctx.restore();
+      this.flipImageBack(mo);
     }
   }
+
+  flipImage(mo) {
+    this.ctx.save();
+    this.ctx.translate(mo.width, 0);
+    this.ctx.scale(-1, 1);
+    mo.x = mo.x * -1;
+  }
+
+  flipImageBack(mo) {
+    mo.x = mo.x * -1;
+    this.ctx.restore();
+}
 }
