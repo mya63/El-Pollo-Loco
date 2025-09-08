e8acab5d005f55613ca649671e7a0f07ab47d742
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-08 18:08:31 +0200

    feat: Schaden zufügen

--- models/world.class.js
+++ models/world.class.js
@@ -23,7 +23,8 @@ class World {
     setInterval(() => {
       this.level.enemies.forEach((enemy) => {
         if (this.character.isCollided(enemy)) {
-          console.log('Collision with Character ' + enemy);
+          this.character.energy -= 2;
+          console.log('Collision with Character,enemy ' + this.character.energy);
         }
       });
     },200);
