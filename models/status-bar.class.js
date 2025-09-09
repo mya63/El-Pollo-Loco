35ac1178177e42c79aad72988cd4a7e6880d5db0
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-09 16:58:39 +0200

    feat: Statusbar

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -11,7 +11,13 @@ class StatusBar extends DrawableObject {
   percentage = 100;
 
   constructor() {
+    super();
     this.loadImages(this.IMAGES);
+    this.x = 40;
+    this.y = 0;
+    this.width = 200;
+    this.height = 60;
+    this.setPercentage(100);
   }
 
   setPercentage(percentage) {
--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,7 @@ class World {
   ctx;
   keyboard;
   camera_x = 0;
+  statusBar = new StatusBar();
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext('2d');
@@ -35,6 +36,7 @@ class World {
     this.ctx.translate(this.camera_x, 0);
 
     this.addObjectsToMap(this.level.backgroundObjects);
+    this.addToMap(this.statusBar);
     this.addToMap(this.character);
     this.addObjectsToMap(this.level.enemies);
     this.addObjectsToMap(this.level.clouds);
