5404e76ba0d1ea1fcbb6261c4ceeba0fa2d25380
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-09-12 00:25:16 +0200

    fix

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -17,7 +17,7 @@ class Endboss extends MovableObject {
     'img/4_enemie_boss_chicken/1_walk/G1.png',
     'img/4_enemie_boss_chicken/1_walk/G2.png',
     'img/4_enemie_boss_chicken/1_walk/G3.png',
-    'img/4_enemie_boss_chicken/1_walk/G4.png'
+    'img/4_enemie_boss_chicken/1_walk/G4.png',
   ];
 
  constructor(){ super().loadImage(this.IMAGES_WALK[0]);
